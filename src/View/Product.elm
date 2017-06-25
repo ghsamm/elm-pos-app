cba28763d2a8ce2c23a87345d4447ace4e5f2f27
ghsamm <ghsamm@gmail.com>
2017-06-25 08:21:40 +0100

    finish View.Product

--- src/View/Product.elm
+++ src/View/Product.elm
@@ -2,8 +2,15 @@ module View.Product exposing (..)
 
 import Data.Product exposing (Product)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Util exposing (formatPrice)
 
 
 view : Product -> Html msg
 view product =
-    text "product"
+    div [ class "product__content" ]
+        [ div [ class "product__price" ]
+            [ text <| formatPrice product.price ]
+        , div [ class "product__name" ]
+            [ text product.name ]
+        ]
