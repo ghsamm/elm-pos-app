ed60698cd22f67b5d36cc4f78e78eea54d8f8e6b
ghsamm <ghsamm@gmail.com>
2017-06-29 02:18:06 +0100

    add static view for View.Numpad

---
+++ src/View/Numpad.elm
@@ -0,0 +1,23 @@
+module View.Numpad exposing (view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view : Html msg
+view =
+    div [ class "numpad__container" ]
+        [ div [ class "numpad__content" ]
+            [ div [ class "numpad__number" ] [ text "1" ]
+            , div [ class "numpad__number" ] [ text "2" ]
+            , div [ class "numpad__number" ] [ text "3" ]
+            , div [ class "numpad__number" ] [ text "4" ]
+            , div [ class "numpad__number" ] [ text "5" ]
+            , div [ class "numpad__number" ] [ text "6" ]
+            , div [ class "numpad__number" ] [ text "7" ]
+            , div [ class "numpad__number" ] [ text "8" ]
+            , div [ class "numpad__number" ] [ text "9" ]
+            , div [ class "numpad__number numpad__number--large" ] [ text "0" ]
+            , div [ class "numpad__number" ] [ text "." ]
+            ]
+        ]
