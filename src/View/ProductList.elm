686b3a2993bf09270903d7a05a5adbefb6f6cc9a
sghanmi <sghanmi@linagora.com>
2017-07-24 22:39:22 +0100

    refactor .product-list to elm-css

--- src/View/ProductList.elm
+++ src/View/ProductList.elm
@@ -1,18 +1,34 @@
 module View.ProductList exposing (view)
 
+import Css exposing (..)
 import Data.Model exposing (Model)
 import Data.Msg exposing (Msg)
 import Data.Product exposing (stringToProductId)
 import Data.ProductStore exposing (ProductStore)
 import Dict exposing (keys)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attributes exposing (..)
 import View.ProductContainer as ProductContainer exposing (view)
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
 
 
 view : ProductStore -> Model -> Html Msg
 view productStore store =
-    div [ class "product-list" ] <|
+    div
+        [ styles
+            [ Css.property "display" "grid"
+            , Css.property "grid-template-columns" "repeat(auto-fit, 120px)"
+            , Css.property "grid-auto-rows" "120px"
+            , Css.property "grid-gap" "5px 10px"
+            , Css.property "grid-column-end" "span 2"
+            ]
+        , Attributes.class "product-list"
+        ]
+    <|
         List.map
             (\productId -> ProductContainer.view productId store)
             (List.map stringToProductId <| keys productStore)
