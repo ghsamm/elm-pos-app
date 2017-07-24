b9911d5761f8b8042ae3034dc3b8b4f08a184fca
sghanmi <sghanmi@linagora.com>
2017-07-24 21:23:19 +0100

    refactor .order-line__unit-price--old to elm-css

--- src/View/OrderLine.elm
+++ src/View/OrderLine.elm
@@ -1,45 +1,61 @@
 module View.OrderLine exposing (view)
 
+import Css exposing (..)
 import Data.Discount exposing (Discount(..), applyDiscount, discountToString)
 import Data.OrderLine as OrderLine exposing (OrderLine, OrderLineErr, OrderLineId)
 import Data.Product as Product exposing (Product)
 import Html exposing (..)
-import Html.Attributes exposing (class, classList)
+import Html.Attributes as Attributes exposing (class, classList)
 import Html.Events exposing (..)
 import Util exposing (formatPrice)
 
 
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
+
+
 viewName : String -> Html msg
 viewName name =
-    div [ class "order-line__product-name" ]
+    div [ Attributes.class "order-line__product-name" ]
         [ Html.text <| name ]
 
 
 viewUnitPriceBeforeDiscount : Float -> Discount -> Html msg
 viewUnitPriceBeforeDiscount price discount =
-    case discount of
-        NoDiscount ->
-            span [ class "order-line__unit-price--new" ] [ text <| formatPrice price ]
+    let
+        attributes =
+            case discount of
+                NoDiscount ->
+                    [ Attributes.class "order-line__unit-price--new" ]
 
-        _ ->
-            span [ class "order-line__unit-price--old" ] [ text <| formatPrice price ]
+                _ ->
+                    [ Attributes.class "order-line__unit-price--old"
+                    , styles
+                        [ textDecoration lineThrough
+                        , fontSize (Css.em 0.8)
+                        , fontWeight normal
+                        ]
+                    ]
+    in
+    span attributes [ Html.text <| formatPrice price ]
 
 
 viewUnitPriceAfterDiscount : Float -> Discount -> Html msg
 viewUnitPriceAfterDiscount price discount =
     case discount of
         NoDiscount ->
-            text ""
+            Html.text ""
 
         _ ->
-            span [ class "order-line__unit-price--new" ] [ text <| formatPrice <| applyDiscount discount price ]
+            span [ Attributes.class "order-line__unit-price--new" ] [ Html.text <| formatPrice <| applyDiscount discount price ]
 
 
 viewUnitPrice : Float -> Discount -> Html msg
 viewUnitPrice price discount =
-    div [ class "order-line__unit-price order-line__text--bold" ]
+    div [ Attributes.class "order-line__unit-price order-line__text--bold" ]
         [ viewUnitPriceBeforeDiscount price discount
-        , text " "
+        , Html.text " "
         , viewUnitPriceAfterDiscount price discount
         ]
 
@@ -54,33 +70,33 @@ viewDiscount discount =
         textData =
             case discount of
                 NoDiscount ->
-                    text ""
+                    Html.text ""
 
                 _ ->
                     span []
-                        [ text "* with a discount of "
-                        , span [ class "order-line__text--bold" ]
-                            [ text <| discountToString discount ]
+                        [ Html.text "* with a discount of "
+                        , span [ Attributes.class "order-line__text--bold" ]
+                            [ Html.text <| discountToString discount ]
                         ]
     in
-    div [ class "order-line__discount" ] [ textData ]
+    div [ Attributes.class "order-line__discount" ] [ textData ]
 
 
 viewTotalPrice : Int -> Float -> Discount -> Html msg
 viewTotalPrice quantity unitPrice discount =
     span
-        [ class "order-line__total-price order-line__text--bold" ]
-        [ text <| formatPrice <| applyDiscount discount <| toFloat quantity * unitPrice ]
+        [ Attributes.class "order-line__total-price order-line__text--bold" ]
+        [ Html.text <| formatPrice <| applyDiscount discount <| toFloat quantity * unitPrice ]
 
 
 viewBottomLine : Int -> Float -> Discount -> Html msg
 viewBottomLine quantity unitPrice discount =
-    div [ class "order-line__bottom-line" ]
-        [ text " x "
+    div [ Attributes.class "order-line__bottom-line" ]
+        [ Html.text " x "
         , span
-            [ class "order-line__text--bold" ]
+            [ Attributes.class "order-line__text--bold" ]
             [ Html.text <| toString quantity ]
-        , text " = "
+        , Html.text " = "
         , viewTotalPrice quantity unitPrice discount
         ]
 
@@ -88,7 +104,7 @@ viewBottomLine quantity unitPrice discount =
 viewInfo : Int -> Float -> Discount -> Html msg
 viewInfo quantity unitPrice discount =
     div
-        [ class "order-line__info order-line__row" ]
+        [ Attributes.class "order-line__info order-line__row" ]
         [ viewDiscount discount
         , viewBottomLine quantity unitPrice discount
         ]
