4e3fbc29362a405c106b77a14d693a788b35056d
sghanmi <sghanmi@linagora.com>
2017-07-24 22:25:08 +0100

    refactor .product__name to elm-css

--- src/View/Product.elm
+++ src/View/Product.elm
@@ -1,25 +1,43 @@
 module View.Product exposing (..)
 
+import Css exposing (..)
 import Data.Product as Product exposing (Product, ProductErr, ProductId)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attributes exposing (..)
 import Html.Events exposing (..)
 import Util exposing (formatPrice)
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
 
 
 view : (ProductId -> msg) -> Result ProductErr Product -> Html msg
 view handleClick product =
     case product of
         Err _ ->
-            text "this product is unavailable"
+            Html.text "this product is unavailable"
 
         Ok product ->
             div
-                [ class "product"
+                [ Attributes.class "product"
                 , onClick <| handleClick (product |> Product.toId)
                 ]
-                [ div [ class "product__price" ]
-                    [ text <| formatPrice (product |> Product.toPrice) ]
-                , div [ class "product__name" ]
-                    [ text (product |> Product.toName) ]
+                [ div [ Attributes.class "product__price" ]
+                    [ Html.text <| formatPrice (product |> Product.toPrice) ]
+                , div
+                    [ styles
+                        [ position absolute
+                        , bottom zero
+                        , left zero
+                        , right zero
+                        , fontSize (Css.em 0.7)
+                        , textAlign center
+                        , padding (px 2)
+                        , backgroundColor (hex "eee")
+                        ]
+                    , Attributes.class "product__name"
+                    ]
+                    [ Html.text (product |> Product.toName) ]
                 ]
