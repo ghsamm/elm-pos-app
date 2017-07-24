5f1169e838e1748657001d4249c4dfcad317c950
sghanmi <sghanmi@linagora.com>
2017-07-24 22:17:13 +0100

    refactor .main-panel to elm-css

--- src/View/MainPanel.elm
+++ src/View/MainPanel.elm
@@ -1,17 +1,25 @@
 module View.MainPanel exposing (view)
 
+import Css exposing (..)
 import Data.Model exposing (Model)
 import Data.Msg exposing (..)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attributes exposing (..)
 import Html.Events exposing (..)
+import SelectList
 import Selector.Product exposing (searchProductSelector)
+import View.Breadcrumb as Breadcrumb
 import View.ProductList as ProductList
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
 
 
 viewProductListSearch : Html Msg
 viewProductListSearch =
-    div [ class "product-search" ]
+    div [ Attributes.class "product-search" ]
         [ input
             [ type_ "text"
             , placeholder "Search for products"
@@ -23,7 +31,17 @@ viewProductListSearch =
 
 view : Model -> Html Msg
 view store =
-    div [ class "main-panel" ]
-        [ viewProductListSearch
+    div
+        [ styles
+            [ Css.property "display" "grid"
+            , Css.property "grid-template-rows" "auto 1fr"
+            , Css.property "grid-template-columns" "1fr auto"
+            , Css.property "grid-gap" "10px"
+            , padding (px 10)
+            ]
+        , Attributes.class "main-panel"
+        ]
+        [ Breadcrumb.view (SelectList.singleton "fsdfsd fs df")
+        , viewProductListSearch
         , ProductList.view (searchProductSelector store) store
         ]
