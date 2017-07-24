9b66bab2ee229ed1e9cd4bf96097fb55c98943d6
sghanmi <sghanmi@linagora.com>
2017-07-24 22:08:51 +0100

    refactor .order-line-list to elm-css

--- src/View/OrderLineList.elm
+++ src/View/OrderLineList.elm
@@ -1,18 +1,28 @@
 module View.OrderLineList exposing (view)
 
+import Css exposing (..)
 import Data.Model exposing (Model)
 import Data.Msg exposing (Msg)
 import Data.OrderLine exposing (stringToOrderLineId)
 import Data.OrderLineStore exposing (OrderLineStore)
 import Dict exposing (keys)
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attributes exposing (..)
 import View.OrderLineContainer as OrderLineContainer exposing (view)
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
 
 
 view : OrderLineStore -> Model -> Html Msg
 view orderLineStore store =
-    div [ class "order-line-list" ] <|
+    div
+        [ styles [ overflowY auto ]
+        , Attributes.class "order-line-list"
+        ]
+    <|
         List.map
             (\orderLineId -> OrderLineContainer.view orderLineId store)
             (List.map stringToOrderLineId <| keys orderLineStore)
