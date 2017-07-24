f351e2510bee2ec57d6343a720e90f42c93908e2
sghanmi <sghanmi@linagora.com>
2017-07-24 20:10:24 +0100

    refactor .main-sidebar to elm-css

--- src/View/MainSidebar.elm
+++ src/View/MainSidebar.elm
@@ -1,15 +1,21 @@
 module View.MainSidebar exposing (view)
 
+import Css exposing (..)
 import Data.Model exposing (Model)
 import Data.Msg exposing (..)
 import Data.OrderLine as OrderLine
 import Dict
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes as Attributes exposing (..)
 import Selector.OrderLine exposing (orderLinePrice)
 import Util exposing (formatPrice)
 import View.Numpad as Numpad
 import View.OrderLineList as OrderLineList
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
 
 
 getTotal : Model -> Float
@@ -28,15 +34,23 @@ getTotal store =
 
 viewTotal : Model -> Html Msg
 viewTotal store =
-    div [ class "order-lines-total" ]
-        [ text "TOTAL : "
-        , text <| formatPrice <| getTotal <| store
+    div [ Attributes.class "order-lines-total" ]
+        [ Html.text "TOTAL : "
+        , Html.text <| formatPrice <| getTotal <| store
         ]
 
 
 view : Model -> Html Msg
 view store =
-    div [ class "main-sidebar" ]
+    div
+        [ Attributes.class "main-sidebar"
+        , styles
+            [ Css.property "display" "grid"
+            , borderRight3 (px 2) solid (hex "eee")
+            , Css.property "grid-template-rows" "1fr 30px 250px"
+            , overflowY Css.hidden
+            ]
+        ]
         [ OrderLineList.view store.orderLines store
         , viewTotal store
         , Numpad.view
