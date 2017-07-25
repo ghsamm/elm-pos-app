031b85535ace96664a6a1c33f9e12936c7591fa0
sghanmi <sghanmi@linagora.com>
2017-07-25 22:42:36 +0100

    remove '0' and '.' from .numpad and refactor it

--- src/View/Numpad.elm
+++ src/View/Numpad.elm
@@ -16,11 +16,7 @@ view =
         renderNumber number =
             div
                 [ styles
-                    [ if number == "0" then
-                        Css.property "grid-column-end" "span 2"
-                      else
-                        Css.property "grid-column-end" "initial"
-                    , Css.property "display" "grid"
+                    [ Css.property "display" "grid"
                     , Css.property "justify-content" "center"
                     , Css.property "align-content" "center"
                     , backgroundColor (hex "BBB")
@@ -34,7 +30,7 @@ view =
             [ Css.property "display" "grid"
             , Css.property "grid-area" "action"
             , Css.property "grid-template-columns" "repeat(3, 50px)"
-            , Css.property "grid-template-rows" "repeat(4, 50px)"
+            , Css.property "grid-template-rows" "repeat(3, 50px)"
             , Css.property "grid-gap" "2px"
             , borderRadius (px 5)
             , color (hex "fff")
@@ -46,15 +42,7 @@ view =
             ]
         , Attributes.class "numpad"
         ]
-        [ renderNumber "1"
-        , renderNumber "2"
-        , renderNumber "3"
-        , renderNumber "4"
-        , renderNumber "5"
-        , renderNumber "6"
-        , renderNumber "7"
-        , renderNumber "8"
-        , renderNumber "9"
-        , renderNumber "0"
-        , renderNumber "."
-        ]
+        (List.range 1 9
+            |> List.map toString
+            |> List.map renderNumber
+        )
