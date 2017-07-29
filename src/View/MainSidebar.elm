d9f008a623ed59dccf5beed704d3bd659d218c4b
sghanmi <sghanmi@linagora.com>
2017-07-29 02:07:08 +0100

    refactor viewOrderActionPanel to own module

--- src/View/MainSidebar.elm
+++ src/View/MainSidebar.elm
@@ -13,6 +13,7 @@ import Util exposing (formatPrice)
 import View.Breadcrumb as Breadcrumb
 import View.Colors as Colors
 import View.Numpad as Numpad
+import View.OrderActionPanel as OrderActionPanel
 import View.OrderLineList as OrderLineList
 
 
@@ -52,73 +53,6 @@ viewTotal store =
         ]
 
 
-viewOrderActionPanel : Model -> Html Msg
-viewOrderActionPanel model =
-    let
-        renderButton : String -> String -> Html Msg
-        renderButton text gridArea =
-            div
-                [ styles
-                    [ Css.property "grid-area" gridArea
-                    , Css.property "display" "grid"
-                    , Css.property "align-content" "center"
-                    , Css.property "justify-content" "center"
-                    , border3 (px 2) solid (hex "fff")
-                    , backgroundColor (hex "bbb")
-                    , borderRadius (px 5)
-                    , fontWeight bold
-                    ]
-                ]
-                [ Html.text text ]
-
-        viewNavigation =
-            div
-                [ styles
-                    [ Css.property "display" "grid"
-                    , Css.property "grid-area" "navigation"
-                    , Css.property "grid-template-rows" "repeat(4, 1fr)"
-                    , Css.property "grid-template-areas" "'next' 'next' '.' 'cancel'"
-                    ]
-                ]
-                [ renderButton "Next" "next"
-                , renderButton "Cancel" "cancel"
-                ]
-
-        viewRightActions =
-            div
-                [ styles
-                    [ Css.property "display" "grid"
-                    , Css.property "gridArea" "action-right"
-                    , Css.property "grid-template-rows" "repeat(4, 1fr)"
-                    , Css.property "grid-template-columns" "1fr 1fr"
-                    , Css.property "grid-template-areas" "'minus plus' '. .' '. .' 'delete delete'"
-                    ]
-                , Attributes.class "order-action-panel__right-actions"
-                ]
-                [ renderButton "-" "minus"
-                , renderButton "+" "plus"
-                , renderButton "Delete" "delete"
-                ]
-    in
-    div
-        [ styles
-            [ Css.property "display" "grid"
-            , Css.property "grid-template-rows" "50px 200px"
-            , Css.property "grid-template-columns" "2fr 1fr 1fr"
-            , Css.property "grid-template-areas" "'breadcrumb breadcrumb breadcrumb' 'action-left action-right navigation'"
-            , Css.property "grid-column-gap" "10px"
-            , backgroundColor Colors.mainBg
-            , padding (px 10)
-            ]
-        , Attributes.class "order-action-panel"
-        ]
-        [ Breadcrumb.view (SelectList.fromLists [ "Edit" ] "Method" [ "Payment" ])
-        , viewNavigation
-        , viewRightActions
-        , Numpad.view
-        ]
-
-
 view : Model -> Html Msg
 view store =
     div
@@ -132,5 +66,5 @@ view store =
         ]
         [ OrderLineList.view store.orderLines store
         , viewTotal store
-        , viewOrderActionPanel store
+        , OrderActionPanel.view
         ]
---
+++ src/View/OrderActionPanel.elm
@@ -0,0 +1,81 @@
+module View.OrderActionPanel exposing (view)
+
+import Css exposing (..)
+import Html exposing (..)
+import Html.Attributes as Attributes exposing (..)
+import SelectList
+import View.Breadcrumb as Breadcrumb
+import View.Colors as Colors
+import View.Numpad as Numpad
+
+
+styles : List Mixin -> Attribute msg
+styles =
+    Css.asPairs >> Attributes.style
+
+
+view : Html msg
+view =
+    let
+        renderButton : String -> String -> Html msg
+        renderButton text gridArea =
+            div
+                [ styles
+                    [ Css.property "grid-area" gridArea
+                    , Css.property "display" "grid"
+                    , Css.property "align-content" "center"
+                    , Css.property "justify-content" "center"
+                    , border3 (px 2) solid (hex "fff")
+                    , backgroundColor (hex "bbb")
+                    , borderRadius (px 5)
+                    , fontWeight bold
+                    ]
+                ]
+                [ Html.text text ]
+
+        viewNavigation =
+            div
+                [ styles
+                    [ Css.property "display" "grid"
+                    , Css.property "grid-area" "navigation"
+                    , Css.property "grid-template-rows" "repeat(4, 1fr)"
+                    , Css.property "grid-template-areas" "'next' 'next' '.' 'cancel'"
+                    ]
+                ]
+                [ renderButton "Next" "next"
+                , renderButton "Cancel" "cancel"
+                ]
+
+        viewRightActions =
+            div
+                [ styles
+                    [ Css.property "display" "grid"
+                    , Css.property "gridArea" "action-right"
+                    , Css.property "grid-template-rows" "repeat(4, 1fr)"
+                    , Css.property "grid-template-columns" "1fr 1fr"
+                    , Css.property "grid-template-areas" "'minus plus' '. .' '. .' 'delete delete'"
+                    ]
+                , Attributes.class "order-action-panel__right-actions"
+                ]
+                [ renderButton "-" "minus"
+                , renderButton "+" "plus"
+                , renderButton "Delete" "delete"
+                ]
+    in
+    div
+        [ styles
+            [ Css.property "display" "grid"
+            , Css.property "grid-template-rows" "50px 200px"
+            , Css.property "grid-template-columns" "2fr 1fr 1fr"
+            , Css.property "grid-template-areas" "'breadcrumb breadcrumb breadcrumb' 'action-left action-right navigation'"
+            , Css.property "grid-column-gap" "10px"
+            , backgroundColor Colors.mainBg
+            , padding (px 10)
+            ]
+        , Attributes.class "order-action-panel"
+        ]
+        [ Breadcrumb.view (SelectList.fromLists [ "Edit" ] "Method" [ "Payment" ])
+        , viewNavigation
+        , viewRightActions
+        , Numpad.view
+        ]
