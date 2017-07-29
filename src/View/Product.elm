2be86b11c6a3d909ded83af7f52a206277ad8c38
sghanmi <sghanmi@linagora.com>
2017-07-29 01:56:11 +0100

    refactor to functions viewProductPrice and viewProductName

--- src/View/Product.elm
+++ src/View/Product.elm
@@ -14,6 +14,35 @@ styles =
     Css.asPairs >> Attributes.style
 
 
+viewProductPrice : Float -> Html msg
+viewProductPrice productPrice =
+    div
+        [ styles
+            [ position absolute
+            , top zero
+            , right zero
+            , fontSize (Css.em 0.8)
+            , padding (px 2)
+            , margin (px 2)
+            , backgroundColor Colors.secondaryBg
+            ]
+        , Attributes.class "product__price"
+        ]
+        [ Html.text <| formatPrice productPrice ]
+
+
+viewProductName : String -> Html msg
+viewProductName productName =
+    div
+        [ styles
+            [ fontSize (Css.em 1.2)
+            , textAlign center
+            ]
+        , Attributes.class "product__name"
+        ]
+        [ Html.text productName ]
+
+
 view : (ProductId -> msg) -> Result ProductErr Product -> Html msg
 view handleClick product =
     case product of
@@ -24,37 +53,14 @@ view handleClick product =
             div
                 [ styles
                     [ position relative
-                    , border3 (px 1) solid (hex "eee")
+                    , backgroundColor Colors.mainBg
+                    , displayFlex
+                    , alignItems center
+                    , justifyContent center
                     ]
                 , Attributes.class "product"
                 , onClick <| handleClick (product |> Product.toId)
                 ]
-                [ div
-                    [ styles
-                        [ position absolute
-                        , top zero
-                        , right zero
-                        , fontSize (Css.em 0.8)
-                        , color (hex "000")
-                        , backgroundColor (hex "eee")
-                        , padding (px 2)
-                        , margin (px 2)
-                        ]
-                    , Attributes.class "product__price"
-                    ]
-                    [ Html.text <| formatPrice (product |> Product.toPrice) ]
-                , div
-                    [ styles
-                        [ position absolute
-                        , bottom zero
-                        , left zero
-                        , right zero
-                        , fontSize (Css.em 0.7)
-                        , textAlign center
-                        , padding (px 2)
-                        , backgroundColor (hex "eee")
-                        ]
-                    , Attributes.class "product__name"
-                    ]
-                    [ Html.text (product |> Product.toName) ]
+                [ viewProductPrice (product |> Product.toPrice)
+                , viewProductName (product |> Product.toName)
                 ]
