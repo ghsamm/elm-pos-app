25ef3b0435d72e18d45bbcec619a89019bc00745
sghanmi <sghanmi@linagora.com>
2017-08-16 12:38:26 +0100

    create .order-line-list-container and move .total into it

--- src/View/MainSidebar.elm
+++ src/View/MainSidebar.elm
@@ -3,51 +3,12 @@ module View.MainSidebar exposing (view)
 import Css exposing (..)
 import Data.Model exposing (Model)
 import Data.Msg exposing (..)
-import Data.OrderLine as OrderLine
 import Data.OrderLineStore as OrderLineStore exposing (OrderLineStoreMsg(..))
-import Dict
 import Html exposing (..)
 import Html.Attributes as Attributes exposing (..)
-import Selector.OrderLine exposing (orderLineListSelector, orderLinePrice, orderLineSelector)
 import Util exposing (formatPrice, styles)
-import View.Colors as Colors
 import View.OrderActionPanel as OrderActionPanel
-import View.OrderLineList as OrderLineList
-
-
-getTotal : Model -> Float
-getTotal model =
-    let
-        lineTotals =
-            Dict.values <|
-                Dict.map
-                    (\str orderLine ->
-                        orderLinePrice (orderLine |> OrderLine.toId) model
-                    )
-                    model.orderLineStore.orderLines
-    in
-    List.foldl (+) 0 lineTotals
-
-
-viewTotal : Model -> Html Msg
-viewTotal model =
-    div
-        [ Attributes.class "order-lines-total slide-fade-in-to-right"
-        , styles
-            [ displayFlex
-            , alignItems center
-            , justifyContent spaceBetween
-            , fontSize (Css.em 1.2)
-            , textAlign right
-            , padding2 (px 5) (px 10)
-            , backgroundColor Colors.mainBg
-            , fontWeight bold
-            , Css.property "animation-delay" "100ms"
-            ]
-        ]
-        [ div [] [ Html.text "TOTAL " ]
-        , div [] [ Html.text <| formatPrice <| getTotal <| model ]
-        ]
+import View.OrderLineListContainer as OrderLineListContainer
 
 
 view : Model -> Html Msg
@@ -56,18 +17,12 @@ view model =
         [ Attributes.class "main-sidebar"
         , styles
             [ Css.property "display" "grid"
-            , Css.property "grid-template-rows" "1fr 40px auto"
+            , Css.property "grid-template-rows" "1fr auto"
             , Css.property "grid-row-gap" "10px"
             , overflowY Css.hidden
             ]
         ]
-        [ OrderLineList.view
-            (\orderLineId -> OrderLineStoreMsg (SelectOrderLine orderLineId))
-            model.orderLineStore.selectedOrderLine
-            (OrderLineStore.orderLineIdList model.orderLineStore
-                |> (\idList -> orderLineListSelector idList model)
-            )
-        , viewTotal model
+        [ OrderLineListContainer.view model
         , OrderActionPanel.view model
             { onNumpadClick = \newQuantity -> OrderLineStoreMsg (SetCurrentOrderLineQuantity newQuantity)
             , onDecrement = OrderLineStoreMsg DecrementCurrentOrderLineQunatity
---
+++ src/View/OrderLineListContainer.elm
@@ -0,0 +1,70 @@
+module View.OrderLineListContainer exposing (..)
+
+import Css exposing (..)
+import Data.Model exposing (Model)
+import Data.Msg exposing (..)
+import Data.OrderLine as OrderLine
+import Data.OrderLineStore as OrderLineStore exposing (OrderLineStoreMsg(..))
+import Dict
+import Html exposing (..)
+import Html.Attributes as Attributes exposing (..)
+import Selector.OrderLine exposing (orderLineListSelector, orderLinePrice)
+import Util exposing (formatPrice, styles)
+import View.Colors as Colors
+import View.OrderLineList as OrderLineList
+
+
+getTotal : Model -> Float
+getTotal model =
+    let
+        lineTotals =
+            Dict.values <|
+                Dict.map
+                    (\str orderLine ->
+                        orderLinePrice (orderLine |> OrderLine.toId) model
+                    )
+                    model.orderLineStore.orderLines
+    in
+    List.foldl (+) 0 lineTotals
+
+
+viewTotal : Model -> Html Msg
+viewTotal model =
+    div
+        [ Attributes.class "order-lines-total slide-fade-in-to-right"
+        , styles
+            [ displayFlex
+            , alignItems center
+            , justifyContent spaceBetween
+            , fontSize (Css.em 1.2)
+            , textAlign right
+            , padding2 (px 5) (px 10)
+            , backgroundColor Colors.mainBg
+            , fontWeight bold
+            , borderTop3 (px 1) solid Colors.secondaryBg
+            , Css.property "animation-delay" "100ms"
+            ]
+        ]
+        [ div [] [ Html.text "TOTAL " ]
+        , div [] [ Html.text <| formatPrice <| getTotal <| model ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ styles
+            [ Css.property "display" "grid"
+            , Css.property "grid-template-rows" "1fr 40px"
+            , overflowY Css.hidden
+            ]
+        , Attributes.class "order-line-list-container"
+        ]
+        [ OrderLineList.view
+            (\orderLineId -> OrderLineStoreMsg (SelectOrderLine orderLineId))
+            model.orderLineStore.selectedOrderLine
+            (OrderLineStore.orderLineIdList model.orderLineStore
+                |> (\idList -> orderLineListSelector idList model)
+            )
+        , viewTotal model
+        ]
