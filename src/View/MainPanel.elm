9829fd984f5e1ed473272e16e3b34a7a3a9e15be
sghanmi <sghanmi@linagora.com>
2017-08-19 01:37:24 +0100

    disable .tab-list and .product-search

--- src/View/MainPanel.elm
+++ src/View/MainPanel.elm
@@ -9,34 +9,9 @@ import Data.ProductStore as ProductStore exposing (ProductStoreMsg(..))
 import Data.TagStore as TagStore
 import Html exposing (..)
 import Html.Attributes as Attributes exposing (..)
-import Html.Events exposing (..)
-import SelectList
 import Util exposing (styles)
 import View.ProductList as ProductList
-import View.TabList as TabList exposing (Tab, TabState(..))
 import View.TagList as TagList
-
-
-viewProductSearch : Html Msg
-viewProductSearch =
-    div
-        [ styles
-            [ Css.property "grid-area" "product-search"
-            , displayFlex
-            ]
-        , Attributes.class "product-search"
-        ]
-        [ input
-            [ styles
-                [ border zero
-                , padding (px 10)
-                ]
-            , type_ "text"
-            , placeholder "Search for products"
-            , onInput (\newTitleFilter -> ProductStoreMsg (SetTitleFilter newTitleFilter))
-            ]
-            []
-        ]
 
 
 view : Model -> Html Msg
@@ -44,23 +19,17 @@ view model =
     div
         [ styles
             [ Css.property "display" "grid"
-            , Css.property "grid-template-rows" "50px 50px 1fr"
-            , Css.property "grid-template-columns" "auto 1fr"
+            , Css.property "grid-template-rows" "50px 1fr"
+            , Css.property "grid-template-columns" "1fr"
             , Css.property
                 "grid-template-areas"
-                "'tab-list tab-list' 'product-search tag-list' 'product-list product-list'"
+                "'tag-list' 'product-list'"
             , Css.property "grid-gap" "10px"
             , overflow Css.hidden
             ]
         , Attributes.class "main-panel"
         ]
-        [ TabList.view <|
-            SelectList.fromLists
-                []
-                (Tab "#1520" Editing)
-                [ Tab "#1521" Waiting, Tab "#1511" Done ]
-        , viewProductSearch
-        , TagList.view
+        [ TagList.view
             { onClickTag = \maybeTagId -> ProductStoreMsg (SetTagFilter maybeTagId)
             , onClickDefaultTag = \_ -> ProductStoreMsg (SetTagFilter Nothing)
             }
